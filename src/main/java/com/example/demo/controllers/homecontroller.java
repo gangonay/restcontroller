dcc97a6bb659bffa319a65e4dad005716fc2c9a2
vaibhavi <gangonayvaibhavi@gmail.com>
2021-12-08 15:39:44 +0530

    this spring we project

---
+++ src/main/java/com/example/demo/controllers/homecontroller.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class homecontroller {
+
+	@RequestMapping(value="/home" ,method=RequestMethod.GET)
+	public String home()
+	{
+		return "Hello welcome to SpringBoot Application";
+	}
+}
